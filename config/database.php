946b65d247dbea0f8842ec25e4a5f5f1b545a28c
BuildTools <unconfigured@null.spigotmc.org>
2023-10-13 14:15:47 +0300

    azure testing

--- config/database.php
+++ config/database.php
@@ -1,6 +1,18 @@
 <?php
+//include_once("../../../../../salasanapiilo/tunnukset-db.php");
 
-const DB_HOST = 'localhost';
-const DB_NAME = 'auth';
-const DB_USER = 'katijo';
-const DB_PASSWORD = 'Tossu0709poila';
+
+const DB_HOST = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
+const DB_USER = $_ENV['DB_USER'] ?? getenv('DB_USER');
+const DB_PASSWORD = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD');
+
+//const DB_HOST = '$db_server_local';
+//const DB_NAME = 'auth';
+//const DB_USER = '$db_username_local';
+//const DB_PASSWORD = '$db_password_local';
+
+/*
+$db_server = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
+$db_username = $_ENV['DB_USER'] ?? getenv('DB_USER');
+$db_password = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD');
+*/
--- public/logout.php
+++ public/logout.php
@@ -2,3 +2,16 @@
 
 require __DIR__ . '/../src/bootstrap.php';
 logout();
+
+/*Jos ongelmaa azuren kanssa: vaihtoehto:
+<?php
+
+session_unset();
+
+session_destroy();
+
+//header("Location: index.php?pages=login");
+echo '<script>window.location.href = "index.php?pages=login";</script>
+exit();
+?>
+*/
